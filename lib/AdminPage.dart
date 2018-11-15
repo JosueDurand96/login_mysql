1db97007ef22411331269ddaf113db0ccb4354a3
JosueDurand96 <josue.durand8@gmail.com>
2018-11-15 17:17:54 -0500

    Listar

--- lib/AdminPage.dart
+++ lib/AdminPage.dart
@@ -1,17 +1,75 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-class AdminPage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:http/http.dart'as http;
+import 'detail.dart';
+class AdminPage extends StatefulWidget {
 
   AdminPage({this.username});
   final String username;
 
   @override
+  _AdminPage createState()=> new _AdminPage();
+
+
+}
+
+// ignore: type_argument_not_matching_bounds
+class _AdminPage extends State<AdminPage> {
+  Future<List> getData() async {
+    final response = await http.get("http://192.168.1.134/Login/getdata.php");
+    return json.decode(response.body);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Welcome Admin "+username),),
+    return new Scaffold(
+        appBar: new AppBar(title: new Text("MY STORE 2")),
 
 
+        body: new FutureBuilder<List>(
+          future: getData(),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) print(snapshot.error);
 
+            return snapshot.hasData
+                ? new ItemList(list: snapshot.data,)
+                : new Center(child: new CircularProgressIndicator(),);
+          },
+        )
     );
   }
 }
+  // ignore: missing_method_parameters, class_in_class, expected_class_member
+  class ItemList extends StatelessWidget{
+  List list;
+  ItemList({this.list});
+   @override
+    Widget build (BuildContext context){
+     return new  ListView.builder(
+     itemCount: list==null?0:list.length,
+       itemBuilder: (context,i){
+       // return new Text(list[i]['item_name']); lista uno por uno los items
+
+         return new Container(
+           padding: const EdgeInsets.all(10.0),
+                child: new GestureDetector(
+                  onTap: ()=>Navigator.of(context).push(
+                    new MaterialPageRoute(
+                        builder: (BuildContext context)=> new Detail(list: list,index:i,))
+                  ),
+                  child: new Card(
+                     child: new ListTile(
+             title: new Text(list[i]['item_name']),
+             leading: new Icon(Icons.widgets),
+             subtitle: new Text("Stock : ${list[i]['stock']}"),
+           ),
+           ),
+                ),
+         );
+       },
+     );
+}
+}
+
---
+++ lib/detail.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class Detail extends StatefulWidget {
+  List list;
+  int index;
+  Detail({this.index,this.list});
+
+  @override
+  _DetailState createState() => _DetailState();
+}
+
+class _DetailState extends State<Detail> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+     appBar: new AppBar(title: new Text("${widget.list[widget.index]['item_name']}")),
+      body: new Container(
+        height: 250.0,
+        padding: const EdgeInsets.all(20.0),
+        child: new Card(
+          child: new Center(
+            child: new Column(
+              children: <Widget>[
+                new Text(widget.list[widget.index]['item_name'],style: new TextStyle(fontSize: 20.0),),
+                new Text("Codigo :  ${widget.list[widget.index]['item_code']}",style: new TextStyle(fontSize: 18.0),),
+                new Text("Precio :  ${widget.list[widget.index]['price']}",style: new TextStyle(fontSize: 18.0),),
+                new Text("Cantidad :  ${widget.list[widget.index]['stock']}",style: new TextStyle(fontSize: 18.0),),
+                new Padding(padding: const EdgeInsets.only(top: 30.0),),
+
+                new Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    new RaisedButton(
+                      child: new Text("EDIT"),
+                        color: Colors.green,
+                        onPressed:(){},
+                    ),
+                    new RaisedButton(
+                      child: new Text("DELETE"),
+                      color: Colors.red,
+                      onPressed:(){},
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ) ,
+      ),
+    );
+  }
+}
